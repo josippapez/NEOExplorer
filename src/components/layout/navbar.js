e52b5eb85c60459998e30bb6ea4afba5de71054a
josippapez <papezjosip@gmail.com>
2019-12-30 15:54:37 +0100

    init commit + added navbar

---
+++ src/components/layout/navbar.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function Navbar(){
+    return(
+        <nav className="navbar navbar-dark bg-dark">
+            <div className="container">
+                <label className='navbar-brand center'>NEO Explorer</label>
+            </div>
+        </nav>
+    )
+}
+
+export default Navbar;
